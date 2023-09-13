6f6a5251abd1f6d731132f9145737895a15be7c3
nsikakudoh <nsikak@sparknerds.io>
2023-09-13 14:29:22 -0600

    template literals

---
+++ strings/script.js
@@ -0,0 +1,10 @@
+const name = "nsikak";
+const age = 32;
+
+x = "hello, my name is " + name + " and i am " + age + " years old";
+
+// template strings
+
+x = `hello, my name is ${name} and i am ${age} years old`;
+
+console.log(x);
